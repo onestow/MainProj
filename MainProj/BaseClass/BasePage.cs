4d4fec73ff055ffa574e6b1e5495246c43127add
unknown <tangyh@efunds-dev.local>
2018-12-19 17:37:44 +0800

    add baseclass

---
+++ MainProj/BaseClass/BasePage.cs
@@ -0,0 +1,14 @@
+﻿using SysCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProj.BaseClass
+{
+    public class BasePage
+    {
+		ThemeManager.SetTheme(this, Theme.MetropolisLight);
+    }
+}
---
+++ MainProj/BaseClass/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using SysCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProj.BaseClass
+{
+    public class RelayCommand<T> : ICommand
+		where T : class
+    {
+		private Predicate<T> canExecute = null;
+		private Action<T> executeAction = null;
+
+		public event EventHandler CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+
+		public RelayCommand(Action<T> executeAction, Predicate<T> canExecute = null)
+		{
+			this.executeAction = executeAction ?? throw new ArgumentException("实现方法不能为空");
+			this.canExecute = canExecute;
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return canExecute?.Invoke(parameter as T) ?? true;
+		}
+
+		public void Execute(object parameter)
+		{
+			executeAction(parameter as T);
+		}
+    }
+}
